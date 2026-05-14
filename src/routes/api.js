56a04fcfdaf4a780588ce5867fba1629bccb5173
EC <me@ceduth.dev>
2026-05-14 12:31:08 -0600

    fix(slack): move slack routes outside auth middleware

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,7 +2,6 @@ const express = require('express');
 const historyController = require('../controllers/historyController');
 const exportsController = require('../controllers/exportsController');
 const statusController = require('../controllers/statusController');
-const slackController = require('../controllers/slackController');
 
 
 function createApiRoutes(pipelineStatus) {
@@ -21,11 +20,6 @@ function createApiRoutes(pipelineStatus) {
 
   // Status routes
   router.get('/status', statusController.getStatus(pipelineStatus));
-
-  // Slack interaction route
-  router.post('/slack/interactions', slackController.handleInteraction);
-  router.post('/slack/events', slackController.handleEvent);
-  router.post('/slack/commands', slackController.handleSlashCommand);
 
   return router;
 }
